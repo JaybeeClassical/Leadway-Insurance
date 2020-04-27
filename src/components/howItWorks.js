3e16668ef44130ea00bf1429cc1bc17fffe70dbc
jaybeeClassical <johnboscoobiejesi@gmail.com>
2020-04-27 03:47:58 +0100

    section added

--- src/components/howItWorks.js
+++ src/components/howItWorks.js
@@ -4,20 +4,20 @@ const HowItWorks = () => {
             <h4>how it works?</h4>
             <div class="how-it-works-box">
                 <div class="box1">
-                    <img src="../asset/box1.svg" alt="box1"/>
-                    <h2>step #1</h2>
+                    <img src="https://res.cloudinary.com/techibytes/image/upload/v1587949807/box1_ojnc2w.svg" class="step1-image" />
+                    <h2>Step #1</h2>
                     <p>Create an account with leadway <br/>
                     assurance if you don't already have one.</p>  
                 </div>
                 <div class="box2">
-                    <div class="image-box2"></div>
-                    <h2>step #2</h2>
+                    <img src="https://res.cloudinary.com/techibytes/image/upload/v1587949807/box1_ojnc2w.svg" class="step2-image" />
+                    <h2>Step #2</h2>
                     <p>Choose your Insurance plan or any of<br/>
                     our offered services</p>
                 </div>
                 <div class="box3">
-                    <div class="image-box1"></div>
-                    <h2>step #3</h2>
+                    <img src="https://res.cloudinary.com/techibytes/image/upload/v1587949807/box2_akbznq.svg" class="step3-image" />
+                    <h2>Step #3</h2>
                     <p>Get a quote by clicking on the get a<br/>
                      quote button or you can give us a call if you aren't clear enough.</p>
                 </div>
